9b10f6136c9aadf38b83b850ade4675b2b9f6ebf
Ola Test <ola.flisback@gmail.com>
2015-06-24 18:26:29 +0200

    Improved viewer and poster + alt post script

--- poster.sh
+++ poster.sh
@@ -3,4 +3,5 @@
 while true; do
     read s
     echo $s >> chat.log
+    git commit -a -m "$s" > /dev/null
 done
--- viewer.sh
+++ viewer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-watch -n 1 --no-title -d "git --no-pager blame chat.log | sed 's/[+-]\{1\}[0-9]\{4\}[ ]*[0-9]*)[ ]//;s/^[[:alnum:]]*[ ]*(//'"
+watch -n 1 --no-title "git --no-pager blame chat.log | sed 's/.*(\([a-zA-Z ]*\) [0-9\-]*[ ]*\([:0-9]*\) [+\-]*[0-9]*[ ]\+[0-9]\+)[ ]*\(.*\)/\2 <\1> \3/' | sed 's/[ ]*>/>/'"
